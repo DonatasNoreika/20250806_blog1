1099ade3ab619e4339b0c56e131a2272b17a1689
DonatasNoreika <donoras@gmail.com>
2025-08-06 18:50:01 +0300

    Pridėtas pagrindinis base.html šablonas

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path("", views.index, name='index'),
     path("register/", views.register, name="register"),
+    path('prisijungti/', LoginView.as_view(template_name='prisijungti.html'), name='prisijungti'),
+    path('atsijungti/', LogoutView.as_view(next_page='prisijungti'), name='atsijungti'),
 ]
