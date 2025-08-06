e718951696c83b10e8279825b7726efc7b8a9139
DonatasNoreika <donoras@gmail.com>
2025-08-06 18:34:33 +0300

    Sukurta vartotojo registracija

--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def index(request):
     return render(request, template_name="index.html")
+
+def register(request):
+    if request.method == "GET":
+        form = UserCreationForm()
+        return render(request, template_name="register.html", context={'form': form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
